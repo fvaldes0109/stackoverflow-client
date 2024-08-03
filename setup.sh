ed1c5e6a339ed320382952d149f0514a4d6f4788
fvaldes0109 <fvaldes0109@gmail.com>
2024-08-03 17:53:18 +0200

    Initial setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+cp .env.example .env
+
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php83-composer:latest \
+    composer install --ignore-platform-reqs
+
+./vendor/bin/sail up -d
+
+./vendor/bin/sail artisan key:generate
+
+./vendor/bin/sail artisan migrate
